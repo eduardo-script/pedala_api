16e587fffcef4715ac1fd77f8db0d834133081f7
Glêsio Santos <glesioss@gmail.com>
2026-07-09 13:51:50 -0300

    cadastrando usuário

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,36 +1,18 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { UsuarioRequestDto } from './dto/usuario_request.dto';
+import { Repository } from 'typeorm';
+import { UsuarioModel } from './usuario.model';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsuarioService {
-    private usuarios = [
-        {
-            nome: "Carlos Oliveira", 
-            email: 'coliveira@mail.com', 
-            contato: '+5586999667788'
-        },
-        {
-            nome: "Sara Alves", 
-            email: 'salves@mail.com', 
-            contato: '+5586999669988'
-        },
-    ]
+    constructor(
+        @InjectRepository(UsuarioModel)
+        private readonly usuarioRepository: Repository<UsuarioModel>
+    ){}
 
-    todosUsuarios() {
-        return this.usuarios
+    async novoUsuario(request: UsuarioRequestDto):Promise<void> {
+        await this.usuarioRepository.save(request)
     }
 
-    buscarUsuarioPorEmail(email:string){
-        const usuario = this.usuarios
-            .find(u => u.email === email)
-        if(!usuario) 
-            throw new NotFoundException("Usuário não encontrado")    
-        return usuario
-    }
-
-    novoUsuario(request: UsuarioRequestDto){
-        const usuario = this.usuarios.find(u => u.email === request.email)
-        if(usuario) throw new BadRequestException("Usuario já cadastrado")
-        this.usuarios.push(request)
-    }
 }
