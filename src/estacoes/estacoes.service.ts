51e71446b294d1d1cd02966cb4142646cd9066dd
Glêsio Santos <glesioss@gmail.com>
2026-07-14 16:51:24 -0300

    Finalizando o cadastro e carregamento das estações

--- src/estacoes/estacoes.service.ts
+++ src/estacoes/estacoes.service.ts
@@ -40,10 +40,9 @@ export class EstacoesService {
     : Promise<EstacaoModel[]>{
         const estacoes = await this.estacaoRepository.find({
             where: {  
-                nomeEstacao: ILike(`${query}%`)
+                nomeEstacao: ILike(`%${query}%`)
             }
         })
-
         return estacoes
     }
 }
