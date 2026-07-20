91598a2f54a3f20b3a38c7660e22ffb498401a1a
Glêsio Santos <glesioss@gmail.com>
2026-07-20 13:36:40 -0300

    desafio manter bicicleta

--- src/bicicleta/bicicleta.controller.ts
+++ src/bicicleta/bicicleta.controller.ts
@@ -1,4 +1,10 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('bicicleta')
-export class BicicletaController {}
+export class BicicletaController {
+
+    // Cadastrar um nova bicicleta informando o Id do modelo e estacao
+    // Deverá validar a capacidade de bicicletas por estações e se a estação esta ativa
+    // Carregar todas as bicicletas da estacação com informando o id da bicicleta, modelos, marca e a estação lotada
+    // Buscar as bibicleta pela estação
+}
