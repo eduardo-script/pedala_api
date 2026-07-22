c2e7125496833ad7e814c802ef60011aefcfa5d7
eduardo-script <eduardovinicius.code@gmail.com>
2026-07-22 15:54:38 -0300

    ultimas atualizações

--- src/marca/dto/marca_request.dto.ts
+++ src/marca/dto/marca_request.dto.ts
@@ -1,10 +1,11 @@
-import { IsNotEmpty, IsOptional } from "class-validator"
+import { IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class MarcaRequestDto {
-    
-    @IsNotEmpty({ message: "O campo nome modelo é obrigatório"})
-    nome: string
+  @IsNotEmpty({ message: 'O nome da marca não pode ser vazio' })
+  @MinLength(2, { message: 'O nome da marca deve ter no mínimo 2 caracteres' })
+  nome: string;
 
-    @IsOptional()
-    url: string
+  @IsOptional()
+  @IsString()
+  urlImagem?: string; // <-- ADICIONE ESTA LINHA AQUI!
 }
